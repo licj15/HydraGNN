66646c97f9c14be12304aebb0843e72b09aca90d
Chaojian Li <thulicj15@gmail.com>
2024-11-19 03:35:44 -0800

    Add a general sbatch scripts and a launch helper script

--- examples/multidataset_deepspeed/job-perlmutter.sh
+++ examples/multidataset_deepspeed/job-perlmutter.sh
@@ -3,11 +3,19 @@
 #SBATCH -J HydraGNN
 #SBATCH -C gpu
 #SBATCH -q regular
-#SBATCH -t 24:00:00
+#SBATCH -t 48:00:00
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-task=1
 #SBATCH -c 32
-#SBATCH -N 128
+
+# Retrieve the number of nodes set via `sbatch -N` or in the script
+echo "Number of nodes allocated: $SLURM_NNODES"
+
+WIDTH=${1:-50}     # Default to 50 hidden_dim if not specified
+DEPTH=${2:-3}      # Default to 3 num_conv_layers if not specified
+DS=${3:-1.2}       # Default to 1.2TB data if not specified
+ZERO=${4:-False}   # Default to False if not specified
+CKPT=${5:-False}   # Default to False if not specified
 
 ## Remove write permission for others in terms of newly created files and dirs
 umask 002
@@ -41,12 +49,25 @@ DATASET_PATH="/global/cfs/projectdirs/m4716/mlupopa/HydraGNN/examples/multidatas
 DATASET_LIST="MPTrj-v3,ANI1x-v3,OC2020-20M-v3,OC2022-v3,qm7x-v3"
 
 ## Log Envs
-LOG_NAME="exp_base-full_data-128-nodes"
+LOG_NAME="exp-${DEPTH}_depth-${WIDTH}_width-${DS}_TB_data-${SLURM_NNODES}_nodes"
+
+## Calculate batch size and num_samples
+BS=$((32 * 32 / SLURM_NNODES)) # Dynamic calculation of batch size, default setting: 32 nodes with 32 batch size per GPU
+NS=$(echo "scale=0; 285715 / 1.2 * ${DS} * 32 / $SLURM_NNODES" | bc) # Calculate number of samples, default setting: 32 nodes with 285715 num_samples per GPU
+
+## Handle optional arguments
+EXTRA_ARGS=""
+if [ "$ZERO" = "True" ]; then
+    EXTRA_ARGS+=" --zero_opt"
+fi
+if [ "$CKPT" = "True" ]; then
+    EXTRA_ARGS+=" --conv_checkpointing"
+fi
 
 ## run scripts
 set -x
 
-srun -N128 -n512 -c32 --ntasks-per-node=4 --gpus-per-task=1 \
+srun -N${SLURM_NNODES} -n$((SLURM_NNODES*4)) -c32 --ntasks-per-node=4 --gpus-per-task=1 \
     python -u $HYDRAGNN_DIR/examples/multidataset_deepspeed/train.py \
         --inputfile=base.json \
         --dataset_path=$DATASET_PATH \
@@ -54,6 +75,12 @@ srun -N128 -n512 -c32 --ntasks-per-node=4 --gpus-per-task=1 \
         --multi_model_list=$DATASET_LIST \
         --num_epoch=10 \
         --everyone --ddstore \
-        --log=$LOG_NAME
+        --log=$LOG_NAME \
+        --hidden_dim=${WIDTH} \
+        --num_conv_layers=${DEPTH} \
+        --full_test \
+        --batch_size=${BS} \
+        --num_samples=${NS} \
+        ${EXTRA_ARGS}
 
 set +x
---
+++ examples/multidataset_deepspeed/launch_helper.py
@@ -0,0 +1,38 @@
+import subprocess
+import argparse
+
+def submit_job(nodes, width, depth, dataset_size, zero=False, ckpt=False):
+    # Command to execute
+    command = ["sbatch", "-N", str(nodes), "job-perlmutter.sh", str(width), str(depth), str(dataset_size), str(zero), str(ckpt)]
+    # Run the command and capture output
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    stdout, stderr = process.communicate()
+    # Extract the job ID
+    output = stdout.strip()
+    job_id = int(output.split()[-1])
+    return job_id
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Submit jobs with varying parameters.")
+    parser.add_argument("--width", type=int, required=True, help="Width of the model.")
+    parser.add_argument("--depth", type=int, required=True, help="Depth of the model.")
+    parser.add_argument("--zero", action="store_true", help="enable zero optimizer with stage 1", default=False)
+    parser.add_argument("--ckpt", action="store_true", help="enable checkpointing for conv layers", default=False)
+    
+    args = parser.parse_args()
+
+    dataset_size_list = [0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2]
+    # nodes_list        = [  4,   8,  16,  16,  32,  32,  32]
+    nodes_list        = [ 32,  32,  32,  32,  32,  32,  32]
+
+    for dataset_size, nodes in zip(dataset_size_list, nodes_list):
+        job_id = submit_job(nodes, args.width, args.depth, dataset_size, args.zero, args.ckpt)
+        print(job_id)
+
+    # nodes = 16
+    # dataset_size = 0.4
+    # for width in [800, 1100, 1700, 2500]:
+    #     print("=== Width: ", width)
+    #     for depth in [4, 5, 6]:
+    #         job_id = submit_job(nodes, width, depth, dataset_size, True, False)
+    #         print(job_id)
